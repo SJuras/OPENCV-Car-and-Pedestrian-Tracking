5efba1e3b94b82df68169d7f8072bb037f6291ca
SJuras <jurasstpko@gmailcom>
2022-01-02 02:47:50 +0100

    v1.1.3 full body detection cascade added.

--- carAndPedestrianTracking.py
+++ carAndPedestrianTracking.py
@@ -1,10 +1,16 @@
 import cv2
 
 img_file = "Assets/carFreeway.jpg"
+video = cv2.VideoCapture(0)
+
 classifier_file = "car_detector.xml"
 
-img = cv2.imread(img_file)
-imgBlack_n_White = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while True:
+    read_successful, frame = video.read()
+    
+
+# img = cv2.imread(img_file)
+# imgBlack_n_White = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # car classifier
 car_tracker = cv2.CascadeClassifier(classifier_file)
